b0c37b6e2c86ab3c6622bf3722ceda0ea2f347b1
choudharylakshyaveer <choudharylakshyaveer@gmail.com>
2026-02-20 18:53:45 +0530

    adding console statement in index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log("APP STARTED");
 root.render(
   <React.StrictMode>
     <App />
